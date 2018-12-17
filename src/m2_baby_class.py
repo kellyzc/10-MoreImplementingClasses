7e25be0047b3ad83385b52db3869d3b4a2117cc8
kellyzc <kellyzc@rose-hulman.edu>
2018-12-17 09:17:18 -0500

    Done

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zach Kelly.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
@@ -107,6 +107,26 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
+
+
+class Baby(object):
+    def __init__(self, name):
+        self.name = name
+        self.time_since_fed = 0
+        print('Hello baby ' + self.name + '!')
+
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name + '.')
+        self.time_since_fed = 0
+
+    def hour_passes(self):
+        if self.time_since_fed == 0:
+            print('Baby ' + self.name + ' is sleeping.')
+        elif self.time_since_fed == 1:
+            print('Baby ' + self.name + ' is awake. Time for food.')
+        else:
+            print('Baby ' + self.name + ' is CRYING uncontrollably! Feed the Baby!')
+        self.time_since_fed = self.time_since_fed + 1
 
 
 # -----------------------------------------------------------------------------
